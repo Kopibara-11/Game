e2bf01807d44579196c564c2227fe9ecb9ff74c7
Kopibara-11 <dumicstas@gmail.com>
2024-12-20 17:33:03 +0200

    Add files via upload

---
+++ Basic_Grass_Biom_things.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Basic_Grass_Biom_things" tilewidth="32" tileheight="20" tilecount="16" columns="4">
+ <image source="Sprout Lands - Sprites - Basic pack/Objects/Basic_Grass_Biom_things.png" width="144" height="80"/>
+</tileset>
